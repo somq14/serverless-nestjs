2105023575d856d8161aff12703ee50be8b2efa4
somq14 <som.q14@gmail.com>
2021-04-14 00:42:53 +0900

    CofigModuleのセットアップ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./configs/${process.env['ENV_NAME']}.env`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
